539e48057f8e893eeae5fad9832b7477f1fb2f3c
Elan Dubrofsky <elan.dubrofsky@gmail.com>
2012-06-09 14:58:43 -0400

    New rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitterDoghouse::Application.config.secret_token = '9b4961b43f06d0083eb3962daf2d6b1e22a879d5a2ac20eb6eb78896620da4274bbfc3ac8b60c74ac36253ac26e80c843d3a659dea1ad5f442ec96a4515bffd8'
