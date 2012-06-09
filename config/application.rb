0faabad07a1dda62b04ae9dbd3c89a622b29fc89
Elan Dubrofsky <elan.dubrofsky@gmail.com>
2012-06-09 16:01:31 -0400

    Add precompile assets

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,7 @@ module TwitterDoghouse
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    config.assets.precompile += %w( *.js *.css )
   end
 end
