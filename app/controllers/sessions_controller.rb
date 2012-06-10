bee33e4c57f382bb44a8a88a40c6b337faefb034
Elan Dubrofsky <elan.dubrofsky@gmail.com>
2012-06-10 02:12:57 -0400

    Put in ability to list all following

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,12 @@ class SessionsController < ApplicationController
     auth = request.env["omniauth.auth"]
     user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
     session[:user_id] = user.id
+    Twitter.configure do |config|
+      config.consumer_key = TWITTER_KEY
+      config.consumer_secret = TWITTER_SECRET
+      config.oauth_token = user.token
+      config.oauth_token_secret = user.secret
+    end
     redirect_to root_url, :notice => "Signed in!"
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  MAX_QUERY_LENGTH = 100
+  
   def self.create_with_omniauth(auth)
     create! do |user|
       user.provider = auth["provider"]
@@ -9,4 +11,28 @@ class User < ActiveRecord::Base
       user.secret = auth['credentials']['secret']
     end
   end
+  
+  def get_following_users
+    following_ids = get_following_ids
+    users = []
+    index = 0
+    while index < following_ids.length
+      users << Twitter.users(following_ids[index...(index+MAX_QUERY_LENGTH)])
+      index += MAX_QUERY_LENGTH
+    end
+    users.flatten
+  end
+  
+  private
+  
+    def get_following_ids
+      following_ids = []
+      cursor = -1
+      while cursor != 0
+        followings = Twitter.friend_ids(nickname, cursor: cursor)
+        following_ids << followings.ids
+        cursor = followings.next_cursor
+      end
+      following_ids.flatten
+    end
 end
